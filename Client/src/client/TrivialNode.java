a27632860baade5776d86cc2cd1a7f1c83549577
Hammurabi Mendes <hmendes@cs.brown.edu>
2011-01-30 12:14:14 -0500

    Inserted new running parameters for helper.

---
+++ Client/src/client/TrivialNode.java
@@ -0,0 +1,11 @@
+package client;
+
+import appspecs.Node;
+
+public class TrivialNode extends Node {
+	private static final long serialVersionUID = 1L;
+
+	public void run() {
+		closeOutputs();
+	}
+}
